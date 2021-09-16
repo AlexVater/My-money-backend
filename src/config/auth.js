effe77065eef1945295b126cc60e42f9e7f9e215
AlexVater <alexsander.vater@gmail.com>
2021-09-16 17:45:25 -0300

    correçoes jwt

--- src/config/auth.js
+++ src/config/auth.js
@@ -1,23 +1,26 @@
-const jwt = require('jsonwebtoken')
-const env = require('../.env')
+const jwt = require("jsonwebtoken");
+const env = require("../.env");
 
 module.exports = (req, res, next) => {
-  if (req.method === 'OPTIONS') {
-    next()
+  if (req.method === "OPTIONS") {
+    next();
   } else {
-    const token = req.body.token || req.query.token || req.headers['authorization']
+    const token =
+      req.body.token || req.query.token || req.headers["authorization"];
 
     if (!token) {
-      return res.status(403).send({errros: ['No token provided.']})
+      return res.status(403).send({ errros: ["No token provided."] });
     }
 
     jwt.verify(token, env.authScret, (err, decoded) => {
       if (err) {
-        return res.status(403).send({erros: ['Failed to authenticate token']})
+        return res
+          .status(403)
+          .send({ erros: ["Failed to authenticate token"], err });
       } else {
-        req.decoded = decoded
-        next()
+        req.decoded = decoded;
+        next();
       }
-    })
+    });
   }
-}
+};
