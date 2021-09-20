ddd3d8b6c1e0d94534f49f32983d1a7c0b678443
AlexVater <alexsander.vater@gmail.com>
2021-09-20 14:56:19 -0300

    finalizaçao do backend

--- src/config/auth.js
+++ src/config/auth.js
@@ -2,21 +2,20 @@ const jwt = require("jsonwebtoken");
 const env = require("../.env");
 
 module.exports = (req, res, next) => {
+  // CORS preflight request
   if (req.method === "OPTIONS") {
     next();
   } else {
     const token =
       req.body.token || req.query.token || req.headers["authorization"];
-
     if (!token) {
-      return res.status(403).send({ errros: ["No token provided."] });
+      return res.status(403).send({ errors: ["No token provided."] });
     }
-
-    jwt.verify(token, env.authScret, (err, decoded) => {
+    jwt.verify(token, env.authSecret, function (err, decoded) {
       if (err) {
-        return res
-          .status(403)
-          .send({ erros: ["Failed to authenticate token"], err });
+        return res.status(403).send({
+          errors: ["Failed to authenticate token."],
+        });
       } else {
         req.decoded = decoded;
         next();
